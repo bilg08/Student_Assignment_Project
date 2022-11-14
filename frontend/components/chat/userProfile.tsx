47bb4624cb53ba3e1e2f6bf344f3d4c671b17290
Bilguun <zorigoobilguun30@gmail.com>
2022-11-14 04:44:15 -0800

    aa

--- frontend/components/chat/userProfile.tsx
+++ frontend/components/chat/userProfile.tsx
@@ -1,16 +1,20 @@
 import { PostButton } from "../ui/postButton";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ColasipbleChatBox } from "./chatBox";
+import { io } from 'socket.io-client';
+
 export const UserProfileBox = (request: any) => {
+
 	const [isChatting, setChatting] = useState(false);
+	const [chatRoom, setChatRoom] = useState('');
 	return (
 		<div>
 			<div className='flex flex-row items-center'>
 				<div className='rounded-full h-12 w-12 border border-black pr-2.5'></div>
-				<div>{request.request.email}</div>
+				<div>{request.request.email&&request.request.email}</div>
 			</div>
-			<div className='pl-'>Дундаж үнэлгээ: {request.request.averageRating}</div>
-			{isChatting ? <ColasipbleChatBox /> : <></>}
+			<div className='pl-'>Дундаж үнэлгээ: {request.request.averageRating&&request.request.averageRating}</div>
+			{isChatting ? <ColasipbleChatBox chatRoomName={chatRoom} /> : <></>}
 			<div className='flex flex-row'>
 				<PostButton
 					data={"Батлах"}
@@ -19,7 +23,10 @@ export const UserProfileBox = (request: any) => {
 				<PostButton
 					data={isChatting ? "Дуусгах" : "Харилцах"}
 					prop={"#FDFD96"}
-					ym={() => setChatting(!isChatting)}
+					ym={() => {
+						setChatRoom(request.request.chatRoomName)
+						setChatting(!isChatting)
+					}}
 				/>
 			</div>
 		</div>
--- frontend/components/receivedpost.tsx
+++ frontend/components/receivedpost.tsx
@@ -1,161 +1,170 @@
 import { PostReceived } from "./ui/postReceived";
 import { PostButton } from "./ui/postButton";
 import { ProfileCard, Button, Chat } from "../components/index";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import axios from "axios";
 import { getCookie } from "cookies-next";
 import { UserProfileBox } from "./chat/userProfile";
 export const ReceivedPosts = () => {
-	const [personalPosts, setPersonalPosts] = useState([
-		{
-			subject: "",
-			detail: "",
-			worker: { id: "", averageRating: "", email: "" },
-			pendingRequest: [{ id: "", averageRating: "", email: "" }],
-		},
-	]);
-	useEffect(() => {
-		const getPersonalData = async () => {
-			const token = getCookie("token");
-			console.log(token);
-			try {
-				const datas = await axios.get("http://localhost:8000/users/posts", {
-					headers: {
-						Authorization: token,
-					},
-				});
-				setPersonalPosts(datas.data.data);
-			} catch (error) {}
-		};
-		getPersonalData();
-	}, []);
-	const buttonArr = [
-		{
-			textValue: "Submit",
-			style: "#BFFCC6",
-			function: () => console.log("submit"),
-		},
-		{
-			textValue: "Cancel",
-			style: "#FFABAB",
-			function: () => console.log("cancel"),
-		},
-	];
-	const postedButtonArr = [
-		{
-			textValue: "Edit",
-			style: "#C4FAF8",
-			function: () => console.log("edit"),
-		},
-		{
-			textValue: "Delete",
-			style: "#FFABAB",
-			function: (el: React.MouseEvent<HTMLButtonElement>) => {
-				const button: HTMLButtonElement = el.currentTarget;
-				const id = button.value;
-				axios
-					.delete(`http://localhost:8000/post/${id}`)
-					.then(function (response) {
-						console.log(response);
-					});
-			},
-		},
-	];
-	const [chosen, setChosen] = useState(true);
-	return (
-		<div className='flex-col items-center lg:w-4/6 md:w-full xs:w-full  m-auto ml-14 overflow-auto h-screen  overscroll-y-none'>
-			<div className=' h-[50px]  pr-2 z-10 bg-white flex justify-between items-end'>
-				<h1 className='text-4xl text-center mt-4'>
-					{chosen ? "Миний зар" : "Хүлээн авсан зар"}
-				</h1>
-				<Button onClick={() => setChosen(!chosen)}>
-					{chosen ? "Хүлээн авсан зар" : "Миний зар"}
-				</Button>
-			</div>
-			{chosen ? (
-				<div className='overscroll-y-none  flex-col flex items-center pb-[100px]'>
-					{personalPosts.map((el, ind) => {
-						return (
-							<ProfileCard key={ind}>
-								<PostReceived
-									name={el.subject}
-									owner={"oruuln"}
-									description={el.detail}
-								/>
-								<div className='flex flex-row flex-wrap'>
-									{postedButtonArr?.map((el, index): any => (
-										<PostButton
-											key={index}
-											data={el.textValue}
-											prop={el.style}
-											ym={el.function}
-										/>
-									))}
-								</div>
-								<div>
-									<h1>Хийх хүсэлтүүд:</h1>
-									{el.pendingRequest.map((request) => {
-										return (
-											<div className=' h-fit lg:w-full md:w-5/6 xs:w-[80%] border border-black rounded-lg flex flex-col p-2'>
-												<UserProfileBox request={request} />
-											</div>
-										);
-									})}
-									{el.worker.id && (
-										<div className='bg-yellow-300 flex flex-col'>
-											<h1>Хийх хүн</h1>
+  const [personalPosts, setPersonalPosts] = useState([
+    {
+      subject: "",
+      detail: "",
+      worker: { id: "", averageRating: "", email: "" },
+      pendingRequest: [{ id: "", averageRating: "", email: "" }],
+    }
+  ]);
+	const [postIInterested,setPostIInterested] = useState()
+  useEffect(() => {
+    const getPostIInterested = async () => {
+      const token = getCookie("token");
+      try {
+        const datas = await axios.get(
+          "http://localhost:8000/post/postToBeDone",
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+		  );
+		setPostIInterested(datas.data.data)
+      } catch (error) {}
+    };
+    getPostIInterested();
+  }, []);
+  useEffect(() => {
+    const getPersonalData = async () => {
+      const token = getCookie("token");
+      try {
+        const datas = await axios.get("http://localhost:8000/users/posts", {
+          headers: {
+            Authorization: token,
+          },
+        });
+        console.log(datas.data);
+        setPersonalPosts(datas.data.data);
+      } catch (error) {}
+    };
+    getPersonalData();
+  }, []);
+  const buttonArr = [
+    {
+      textValue: "Submit",
+      style: "#BFFCC6",
+      function: () => console.log("submit"),
+    },
+    {
+      textValue: "Cancel",
+      style: "#FFABAB",
+      function: () => console.log("cancel"),
+    },
+  ];
+  const postedButtonArr = [
+    {
+      textValue: "Edit",
+      style: "#C4FAF8",
+      function: () => console.log("edit"),
+    },
+    {
+      textValue: "Delete",
+      style: "#FFABAB",
+      function: (el: React.MouseEvent<HTMLButtonElement>) => {
+        const button: HTMLButtonElement = el.currentTarget;
+        const id = button.value;
+        axios
+          .delete(`http://localhost:8000/post/${id}`)
+          .then(function (response) {
+            console.log(response);
+          });
+      },
+    },
+  ];
+  const [chosen, setChosen] = useState(true);
+  return (
+    <div className="flex-col items-center lg:w-4/6 md:w-full xs:w-full  m-auto ml-14 overflow-auto h-screen  overscroll-y-none">
+      <div className=" h-[50px]  pr-2 z-10 bg-white flex justify-between items-end">
+        <h1 className="text-4xl text-center mt-4">
+          {chosen ? "Миний зар" : "Хүлээн авсан зар"}
+        </h1>
+        <Button onClick={() => setChosen(!chosen)}>
+          {chosen ? "Хүлээн авсан зар" : "Миний зар"}
+        </Button>
+      </div>
+      {chosen ? (
+        <div className="overscroll-y-none  flex-col flex items-center pb-[100px]">
+          {personalPosts.map((el, ind) => {
+            return (
+              <ProfileCard key={ind}>
+                <PostReceived
+                  name={el.subject}
+                  owner={"oruuln"}
+                  description={el.detail}
+                />
+                <div className="flex flex-row flex-wrap">
+                  {postedButtonArr?.map((el, index): any => (
+                    <PostButton
+                      key={index}
+                      data={el.textValue}
+                      prop={el.style}
+                      ym={el.function}
+                    />
+                  ))}
+                </div>
+                <div>
+                  <h1>Хийх хүсэлтүүд:</h1>
+                  {el.pendingRequest.map((request) => {
+                    return (
+                      <div className=" h-fit lg:w-full md:w-5/6 xs:w-[80%] border border-black rounded-lg flex flex-col p-2">
+                        <UserProfileBox request={request} />
+                      </div>
+                    );
+                  })}
+                  {el.worker.id && (
+                    <div className="bg-yellow-300 flex flex-col">
+                      <h1>Хийх хүн</h1>
 
-											<div className='flex items-center justify-around'>
-												{el.worker.email}{" "}
-												<PostButton
-													data={"Харилцах"}
-													prop={"rgb(225 29 72)"}
-												/>
-											</div>
-										</div>
-									)}
-								</div>
-							</ProfileCard>
-						);
-					})}
-				</div>
-			) : (
-				<div className='overscroll-y-none  flex-col flex items-center pb-[100px]'>
-					{personalPosts.map((el, ind) => {
-						return (
-							<ProfileCard key={ind}>
-								<PostReceived
-									name={el.subject}
-									owner={"oruuln"}
-									description={el.detail}
-								/>
-								<div className='flex flex-row flex-wrap h-fit items-end'>
-									{buttonArr?.map((el, index): any => {
-										return (
-											<PostButton
-												key={index}
-												data={el.textValue}
-												prop={el.style}
-												ym={el.function}
-											/>
-										);
-									})}
-								</div>
-								<div>
-									{el.pendingRequest.map((request) => {
-										return (
-											<div>
-												<p>{request.email}</p>
-												<p>{request.averageRating}</p>
-											</div>
-										);
-									})}
-								</div>
-							</ProfileCard>
-						);
-					})}
-				</div>
-			)}
-		</div>
-	);
+                      <div className="flex items-center justify-around">
+                        {el.worker.email}{" "}
+                        <PostButton data={"Харилцах"} prop={"rgb(225 29 72)"} />
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </ProfileCard>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="overscroll-y-none  flex-col flex items-center pb-[100px]">
+		{/* {postIInterested.map((el, ind) => {
+            return (
+              <ProfileCard key={ind}>
+                <PostReceived
+                  name={el.subject}
+                  owner={"oruuln"}
+                  description={el.detail}
+                />
+                <div className="flex flex-row flex-wrap h-fit items-end">
+                  {buttonArr?.map((el, index): any => {
+                    return (
+                      <PostButton
+                        key={index}
+                        data={el.textValue}
+                        prop={el.style}
+                        ym={el.function}
+                      />
+                    );
+                  })}
+					</div>
+					<div className=" h-fit lg:w-full md:w-5/6 xs:w-[80%] border border-black rounded-lg flex flex-col p-2">
+						<UserProfileBox request={el} />
+                    </div>
+            
+              </ProfileCard>
+            );
+          })} */}
+        </div>
+      )}
+    </div>
+  );
 };
